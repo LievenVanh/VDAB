e5a2c7c036743584c25edc7b581825fa7446bc76
Lieven Vanhercke <lieven.vanh@gmail.com>
2014-08-25 11:40:30 +0200

    Finished opdr10

---
+++ 10 Totale saldo per klant/Bank.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\user\Dropbox\Visual Studio\Projects\Entity Framework\10 Totale saldo per klant\Bank.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
--- 10 Totale saldo per klant/Program.cs
+++ 10 Totale saldo per klant/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace _10_Totale_saldo_per_klant
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Totale saldo per klant:");
+            using (var bankEntities = new BankEntities())
+            {
+                var query = from saldoKlant in bankEntities.TotaleSaldoPerKlant
+                    orderby saldoKlant.Voornaam
+                    select saldoKlant;
+                foreach (var saldoKlant in query)
+                {
+                    Console.WriteLine("{0}: {1}",saldoKlant.Voornaam, saldoKlant.TotaleSaldo);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
