79f613de76ba3123c32a888c8c0abcecbe28afb2
Lieven Vanhercke <lieven.vanh@gmail.com>
2014-08-25 11:32:30 +0200

    added new projects

---
+++ 10 Totale saldo per klant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Totale_saldo_per_klant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
