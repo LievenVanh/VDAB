a0283d89e227f9e81ca644ceda48ec1be6d21513
Lieven Vanhercke <lieven.vanh@gmail.com>
2014-08-25 11:50:43 +0200

    removed unused references

--- 11 Administratieve kost/Program.cs
+++ 11 Administratieve kost/Program.cs
@@ -10,6 +10,21 @@ namespace _11_Administratieve_kost
     {
         static void Main(string[] args)
         {
+            Console.Write("Geef de administratieve kost in: ");
+            decimal adminKost;
+            if (decimal.TryParse(Console.ReadLine(), out adminKost))
+            {
+                using (var bankEntities = new BankEntities())
+                {
+                    bankEntities.AdministratieveKost(adminKost);
+                    Console.WriteLine("Administratieve kost toegepast");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Geef een geldig bedrag in");
+            }
+            Console.ReadLine();
         }
     }
 }
